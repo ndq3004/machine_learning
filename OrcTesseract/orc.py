8bc6b4dea21070531cc480ef6ba44429ce06bad8
ndq3004 <ndq3004@gmail.com>
2020-10-09 13:52:37 +0700

    add some file

---
+++ OrcTesseract/orc.py
@@ -0,0 +1,31 @@
+from PIL import Image
+import pytesseract
+import argparse
+import cv2
+import os
+
+ap = argparse.ArgumentParser()
+ap.add_argument("-i", "--image", required=True, help="Path to input Image")
+ap.add_argument("-p", "--preprocess", type= str, default="thresh", 
+                help="Type of preprocessing")
+args = vars(ap.parse_args())
+
+#load the image
+image = cv2.imread(args['image'])
+gray  = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
+
+#check the preprocess
+if args['preprocess'] == 'thresh':
+    gray = cv2.threshold(gray, 0, 255, cv2.THRESH_BINARY | cv2.THRESH_OTSU)[1]
+elif args['preprocess'] == 'blur':
+    gray = cv2.medianBlur(gray, 3)
+
+filename = "{}.png".format(os.getpid())
+cv2.imwrite(filename, gray)
+
+text = pytesseract.image_to_string(Image.open(filename))
+os.remove(filename)
+print(text)
+cv2.imshow("Image: ", image)
+cv2.imshow("Output: ", gray)
+cv2.waitKey(0)
