77b00a567008c41c782ba90a95aea53285fff06f
ndq3004 <ndq3004@gmail.com>
2020-10-09 14:03:06 +0700

    commit deleted

--- keras_mnist.py
+++
@@ -1,4 +0,0 @@
-from sklearn.preprocessing import LabelBinarizer
-from sklearn.model_selection import train_test_split
-from sklearn.metrics import classification_report
-from keras.models import Sequential
